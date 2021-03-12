11f451eb71ed53b5e34cd94c130a66be8ce50c83
hugosimao10 <61155165+hugosimao10@users.noreply.github.com>
2021-03-12 19:51:06 +0000

    floating button - mapa

--- app/src/main/java/ipvc/estg/auxiliocidadao/Mapa.kt
+++ app/src/main/java/ipvc/estg/auxiliocidadao/Mapa.kt
@@ -2,10 +2,19 @@ package ipvc.estg.auxiliocidadao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
 
 class Mapa : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mapa)
+
+        val fab: View = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+        }
     }
 }
