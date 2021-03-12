a80a6ae28af488e01fa4fb64f40cb9eb831b892e
hugosimao10 <61155165+hugosimao10@users.noreply.github.com>
2021-03-12 19:41:43 +0000

    Criação das activitys todas, multilingua e 2 layouts de landscape

---
+++ app/src/main/java/ipvc/estg/auxiliocidadao/AddNotasPessoais.kt
@@ -0,0 +1,11 @@
+package ipvc.estg.auxiliocidadao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNotasPessoais : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_notas_pessoais)
+    }
+}
--- app/src/main/java/ipvc/estg/auxiliocidadao/MainActivity.kt
+++ app/src/main/java/ipvc/estg/auxiliocidadao/MainActivity.kt
@@ -1,11 +1,20 @@
 package ipvc.estg.auxiliocidadao
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.buttonLogin)
+        button.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
     }
 }
