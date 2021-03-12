49fbfe191e457810bde609fab362ed07a6c48d6f
hugosimao10 <61155165+hugosimao10@users.noreply.github.com>
2021-03-12 20:08:40 +0000

    notas pessoais feito

--- app/src/main/java/ipvc/estg/auxiliocidadao/NotasPessoais.kt
+++ app/src/main/java/ipvc/estg/auxiliocidadao/NotasPessoais.kt
@@ -1,11 +1,20 @@
 package ipvc.estg.auxiliocidadao
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
 
 class NotasPessoais : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notas_pessoais)
+
+        val fab: View = findViewById(R.id.fab1)
+        fab.setOnClickListener { view ->
+            val intent = Intent(this, AddNotasPessoais::class.java)
+            startActivity(intent)
+        }
     }
 }
