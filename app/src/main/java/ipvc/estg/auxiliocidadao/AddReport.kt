cdecc38bac6025d16904ba5a541a9e233e5da248
hugosimao10 <61155165+hugosimao10@users.noreply.github.com>
2021-04-19 23:09:01 +0100

    Adicionado botao voltar no AddReport

--- app/src/main/java/ipvc/estg/auxiliocidadao/AddReport.kt
+++ app/src/main/java/ipvc/estg/auxiliocidadao/AddReport.kt
@@ -1,14 +1,20 @@
 package ipvc.estg.auxiliocidadao
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add_report.*
 
 class AddReport : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_report)
 
+        fab_voltar2.setOnClickListener { _ ->
+            val intent = Intent(this, Maps::class.java)
 
+            startActivity(intent)
+        }
 
 
     }
