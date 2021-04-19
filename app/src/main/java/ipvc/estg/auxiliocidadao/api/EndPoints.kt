0da33ce8b87996b61c5d5044934cd3a15e923d52
hugosimao10 <61155165+hugosimao10@users.noreply.github.com>
2021-04-20 00:13:02 +0100

    Url de insert report pronto

--- app/src/main/java/ipvc/estg/auxiliocidadao/api/EndPoints.kt
+++ app/src/main/java/ipvc/estg/auxiliocidadao/api/EndPoints.kt
@@ -2,6 +2,7 @@ package ipvc.estg.auxiliocidadao.api
 
 import retrofit2.Call
 import retrofit2.http.*
+import java.sql.Blob
 
 interface EndPoints {
 
@@ -14,5 +15,11 @@ interface EndPoints {
     @FormUrlEncoded
     @POST("api/users/login")
     fun getUsers(@Field("username") first: String?, @Field("password")second: String?): Call<OutputPost>
+
+    @FormUrlEncoded
+    @POST("api/reports/insert")
+    fun setReport(@Field("problem") first: String?, @Field("lat")second: Double,  @Field("lng")third: Double,
+                  @Field("lng")fourth: Double, @Field("photo")fifth: Blob, @Field("users_id")sixth: Int): Call<Report>
+
 
 }
